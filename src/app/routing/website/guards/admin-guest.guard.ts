c6650167f1bbddefaed23301c9be5d9b01e1dba4
Sergey <kurilovsergey15@gmail.com>
2022-04-07 15:43:59 +0300

    #6 routing & guards

---
+++ src/app/routing/website/guards/admin-guest.guard.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AdminGuestGuard implements CanActivate, CanLoad {
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return confirm('Are you guest (canActivate)?');
+  }
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
+    return confirm('Are you guest? (canLoad)');
+  }
+}
--- src/app/routing/website/website.module.ts
+++ src/app/routing/website/website.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule} from '@angular/router';
+import {AdminGuestGuard} from './guards/admin-guest.guard';
+import {AdminAuthGuard} from './guards/admin-auth.guard';
 
 @NgModule({
   declarations: [],
@@ -14,12 +16,26 @@ import {RouterModule} from '@angular/router';
           .then(module => module.HomeModule)
       },
       {
+        path: 'admin/auth',
+        loadChildren: () => import('./routing/admin-auth/admin-auth.module')
+          .then(module => module.AdminAuthModule),
+        canLoad: [AdminGuestGuard],
+        canActivate: [AdminGuestGuard]
+      },
+      {
+        path: 'admin',
+        loadChildren: () => import('./routing/admin/admin.module')
+          .then(module => module.AdminModule),
+        canLoad: [AdminAuthGuard],
+        canActivate: [AdminAuthGuard]
+      },
+      {
         path: '**',
         loadChildren: () => import('./routing/not-found/not-found.module')
           .then(module => module.NotFoundModule)
       }
     ])
   ],
-  providers: []
+  providers: [AdminGuestGuard, AdminAuthGuard]
 })
 export class WebsiteModule { }
